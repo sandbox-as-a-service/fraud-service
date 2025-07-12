307339644f270445de88b172e012f8d1b3c70b64
James <john.usa.james@gmail.com>
2025-07-12 15:40:22 +0100

    refactor: change import to use 'type' for VercelRequest and VercelResponse

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import { VercelRequest, VercelResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 
 export default async function handler(
   _req: VercelRequest,
